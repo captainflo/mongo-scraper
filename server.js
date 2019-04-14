c41fa355e5eb243516437a3cb713e4941f1c18fe
florian lahitte <lahitte.florian@gmail.com>
2019-04-13 20:24:17 -0400

    fix dababase

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// // Connect to the Mongo DB
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-mongoose.connect("mongodb://heroku_zff3bnw1:7no24iunfu15m01sgsgpdhqj8i@ds139956.mlab.com:39956/heroku_zff3bnw1");
-
-var PORT = 3000;
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 // Initialize Express
 var app = express();
 
 // Configure middleware
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true });
+}
+// mongoose.connect("mongodb://heroku_zff3bnw1:7no24iunfu15m01sgsgpdhqj8i@ds139956.mlab.com:39956/heroku_zff3bnw1");
+var db = mongoose.connection;
+db.on('error',function(err){
+    console.log('Mongoose Error',err);
+});
+db.once('open', function(){
+    console.log("Mongoose connection is successful");
+});
 
 // Routes
 
 
+
 // Start the server
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
-
-
